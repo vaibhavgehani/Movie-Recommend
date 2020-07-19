4ad43bae4ed387ce719da7b1a8e52642376d3c02
vaibhav gehani <vgehani90@gmail.com>
2020-07-19 11:41:20 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,18 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule],
   providers: [
     StatusBar,
     SplashScreen,
+    HttpClient,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../environments/environment';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  MovieName = '';
+  url = environment.modelUrl;
+  public recommentList: any = [{title: "Gordy"},{title: "The Black Cauldron"},{title: "A Close Shave"},{title: "City Slickers II: The Legend of Curly's Gold"},{title: "The Newton Boys"}]
+  constructor(private http: HttpClient) {}
+  getRecommend() {
+    if (this.MovieName !== '') {
+      this.http.get(`${this.url}?name=${this.MovieName}`).subscribe((response) => {
+        console.log(response);
+        this.recommentList = response;
+        this.recommentList.forEach(element => {
+          element.poster_path = 'http://image.tmdb.org/t/p/w92' + element.poster_path;
+        });
+      });
+    }
+  }
+  clearField() {
+    this.MovieName = '';
+    this.recommentList = [];
+  }
 }
